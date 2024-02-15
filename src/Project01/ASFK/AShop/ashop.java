f6ae283563c5d4716d8147f2428ae770ecfd0d39
ejzus88 <ejzus88@naver.com>
2024-02-16 00:13:16 +0900

    신세계프로젝트입니다.

---
+++ src/Project01/ASFK/AShop/ashop.java
@@ -0,0 +1,11 @@
+package Project01.ASFK.AShop;
+
+public class ashop {
+	public static void main(String[] args) {
+		
+		System.out.println("정진만은 죽었는가?");
+	
+		
+	}
+
+}
---
+++ src/Project01/ASFK/Killer/killer.java
@@ -0,0 +1,7 @@
+package Project01.ASFK.Killer;
+
+public class killer {
+	public static void main(String[] args) {
+		
+	}
+}
